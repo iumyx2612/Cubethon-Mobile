704232d871912b9e84e74263e9a84129377dd82a
Nguyen Mai <DeftNotTroanh@gmail.com>
2020-08-15 01:52:22 +0700

    Coin

---
+++ Assets/CoinData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinData : MonoBehaviour
+{
+    public int coinCounter;
+    public Text coinText;
+
+    private void Start()
+    {
+        coinCounter = 0;
+    }
+
+    private void Update()
+    {
+        coinText.text = coinCounter.ToString();
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(180 * Time.deltaTime, 180f * Time.deltaTime, 180 * Time.deltaTime);
+    }
+}
