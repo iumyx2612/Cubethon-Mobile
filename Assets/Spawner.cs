83661c0e7c06dfd36acc948958be2999b863fad8
Nguyen Mai <DeftNotTroanh@gmail.com>
2020-08-16 21:44:45 +0700

    Spawn Plane (1)

---
+++ Assets/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject plane;
+    private GameObject player;
+    private Vector3 playerPos;
+    public GameObject planes;
+    private float PlaneZ;
+    private float planeLength = 200;
+    private int amountOfPlaneonScreen = 5;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        for (int i = 0; i < amountOfPlaneonScreen; i++) 
+        {
+            SpawnPlane();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPlane()
+    {
+        GameObject gameObject = Instantiate(plane) as GameObject;
+        gameObject.transform.SetParent(planes.transform);
+        gameObject.transform.position = Vector3.forward * PlaneZ;
+        PlaneZ += planeLength;
+    }
+}
