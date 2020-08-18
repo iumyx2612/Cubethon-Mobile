11df0b61074111dc6c893ef572c123d89ddbb776
Nguyen Mai <DeftNotTroanh@gmail.com>
2020-08-18 14:06:01 +0700

    Fixing Spawner

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -14,18 +14,21 @@ public class Spawner : MonoBehaviour
     private GameObject player;
 
     public GameObject[] obstacleSet;
-    private GameObject obstacleContainer;
+    public GameObject obstacleContainer;
     [SerializeField]
     private int min_distance_btw_obstacleset;
     [SerializeField]
     private int max_distance_btw_obstacleset;
+    private float obstacle_set_length = 50f;
     [SerializeField]
     private int amount_of_obstacleset_on_scr;
+    private List<GameObject> activeObstacles;
 
     // Start is called before the first frame update
     void Start()
     {
         activePlanes = new List<GameObject>();
+        activeObstacles = new List<GameObject>();
         player = GameObject.FindGameObjectWithTag("Player");        
         for (int i = 0; i < amountOfPlaneonScreen; i++) 
         {
@@ -36,7 +39,7 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.z - 110f > (totalLength - amountOfPlaneonScreen * planeLength))
+        if (player.transform.position.z - 110f > (totalLength - amountOfPlaneonScreen * planeLength))
         {
             SpawnPlane();
             DeletePlane();
@@ -50,6 +53,29 @@ public class Spawner : MonoBehaviour
         newPlane.transform.position = new Vector3(0, 0, 1) * totalLength;
         totalLength += planeLength;
         activePlanes.Add(newPlane);
+
+        while (obstacle_set_length < totalLength)
+        {
+            GameObject new_obstacle_set;
+            int random_obstacle = (int)Random.Range(0, obstacleSet.Length);
+            if (random_obstacle == 0 || random_obstacle == 1)
+            {
+                float min_x = -5.6f;
+                float max_x = 5.6f;
+                new_obstacle_set = Instantiate(obstacleSet[random_obstacle], new Vector3(Random.Range(min_x, max_x), -0.5f, transform.position.z), Quaternion.identity);
+                Debug.Log(new_obstacle_set.transform.position);
+            }
+            else
+            {
+                new_obstacle_set = Instantiate(obstacleSet[random_obstacle]);
+            }
+            new_obstacle_set.transform.SetParent(obstacleContainer.transform);
+            new_obstacle_set.transform.position = Vector3.forward * obstacle_set_length;
+            obstacle_set_length += GenerateDistance();
+        }
+
+
+
     }
 
     void DeletePlane()
@@ -58,17 +84,9 @@ public class Spawner : MonoBehaviour
         activePlanes.RemoveAt(0);
     }
 
-    void SpawnObstacleSet()
+    float GenerateDistance()
     {
-        GameObject new_obstacle_set;
-        new_obstacle_set = Instantiate(obstacleSet[Random.Range(0, obstacleSet.Length)]);
-        new_obstacle_set.transform.SetParent(obstacleContainer.transform);
-
-    }
-
-    int GenerateDistance()
-    {
-        int temp;
+        float temp;
         temp = Random.Range(min_distance_btw_obstacleset, max_distance_btw_obstacleset);
         return temp;
     }
